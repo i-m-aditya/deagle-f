db69daf8a9be80c62595084794188e926d97038f
Aditya Pandey <aditya@Adityas-MacBook-Air.local>
2021-08-26 23:25:49 +0530

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import background from "./images/websiteBg10.png";
 
 function App() {
   return (
     <div class=" absolute">
-      <img className="object-cover" src={background} />
+      <img className="object-cover" src={background} alt=""/>
       <div className="absolute top-1/3 left-0 right-0">
         <div className=" text-black font-serif text-opacity-70 text-9xl font-extrabold text-center break-normal"> Deagle Protocol</div>
-        {/* <div className="text-white text-opacity-50 text-bold font-extrabold absolute right-0"> NFTs Options Protocol</div> */}
         <div className="text-black font-serif text-opacity-80 text-2xl font-extrabold text-center mt-10"> Coming Soon !</div>
         <div className="text-black font-serif text-opacity-80 text-bold font-extrabold text-center mt-10"> contact@deagle.finance</div>
       </div>
